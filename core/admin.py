8108d10d47c71e5d44ba022d0151c8cc6e7d36b5
YawOtuo <yotuo2003@gmail.com>
2024-07-02 17:30:24 +0000

    "Updated ProductImageInline and ProductVariantInline in admin.py"

--- core/admin.py
+++ core/admin.py
@@ -9,11 +9,15 @@ class ProductImageInline(admin.TabularInline):
     model = ProductImage
     ProductImage
     extra = 1
+    list_display = ['id', 'image', 'is_main']  # Customize displayed columns
+    fields = ['image', 'is_main']  # Specify editable fields for ProductImage creation/editing
+    # i dont
+
 
 class ProductVariantInline(admin.StackedInline):
     model = ProductVariant
     extra = 1
-    inlines = [ProductImageInline]  # Include ProductImageInline within ProductVariantInline
+    # inlines = [ProductImageInline]  # Include ProductImageInline within ProductVariantInline
 
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageInline, ProductVariantInline]
