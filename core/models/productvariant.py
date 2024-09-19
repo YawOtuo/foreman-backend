4c1a260233b0823e1b0d5721539f2d964dd7d6bd
YawOtuo <yotuo2003@gmail.com>
2024-09-19 14:21:08 +0000

    "Refactor ProductVariant model: reformatted code, added blank=True to sku field, and wrapped long lines."

--- core/models/productvariant.py
+++ core/models/productvariant.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 
-
 class ProductVariant(models.Model):
-    product = models.ForeignKey("core.Product", on_delete=models.CASCADE, related_name="variants")
-    sku = models.CharField(max_length=100, unique=True, null=True)
+    product = models.ForeignKey(
+        "core.Product", on_delete=models.CASCADE, related_name="variants"
+    )
+    sku = models.CharField(max_length=100, unique=True, null=True, blank=True)
     name = models.CharField(max_length=255, null=False)
     brief_description = models.CharField(max_length=255, blank=True, null=True)
     detailed_description = models.TextField(blank=True, null=True)
@@ -13,18 +14,20 @@ class ProductVariant(models.Model):
     width = models.CharField(max_length=100, blank=True, null=True)
     # price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     availability = models.CharField(max_length=50, blank=True)
-    related_products = models.ManyToManyField("core.Product", related_name='related_products', blank=True)
+    related_products = models.ManyToManyField(
+        "core.Product", related_name="related_products", blank=True
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name} ({self.sku})"
-    
+
     @property
     def min_order_quantity(self):
         return self.product.category.min_order_quantity
-    
+
     @property
     def min_order_value(self):
         return self.product.category.min_order_value
